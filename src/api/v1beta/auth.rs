535b9989b4a3e14d060b0a5136199a3f9ab6cda7
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-13 18:27:31 -0400

    auth routes

--- src/api/v1beta/auth.rs
+++ src/api/v1beta/auth.rs
@@ -1,1 +1,14 @@
+use rocket::serde::json::Json;
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+pub struct RegisterBody<'r> {
+    username: &'r str,
+    password: &'r str,
+}
+
+#[post("/register", data = "<body>")]
+pub fn register(body: Json<RegisterBody<'_>>) {}
+
+#[post("/login")]
+pub fn login() {}
