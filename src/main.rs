820a6aeef0583c62cb61af1254103acfcfe2cd17
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-12 20:00:46 -0400

    rocket setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![allow(unused)]
 #![allow(dead_code)]
 
-fn main() {
-    println!("こんにちは世界！");
+pub mod api;
+
+#[macro_use]
+extern crate rocket;
+
+#[launch]
+fn launch() -> _ {
+    rocket::build().register("/", catchers![api::handlers::not_found])
 }
