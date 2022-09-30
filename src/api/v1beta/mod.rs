b94fc4e3a45caff5ad93f19269889fc8771c3440
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-30 16:20:45 -0400

    listen on lan + health endpoint

--- src/api/v1beta/mod.rs
+++ src/api/v1beta/mod.rs
@@ -1,10 +1,16 @@
-use rocket::Route;
+use rocket::{Route, http::Status};
 
 pub mod auth;
 pub mod device;
 
+#[get("/health")]
+fn health() -> Status {
+    Status::Ok
+}
+
 pub fn routes() -> Vec<Route> {
     routes![
+        health,
         auth::register,
         auth::login,
         device::register,
